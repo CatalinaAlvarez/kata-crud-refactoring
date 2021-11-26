e9799bd93e003c8a62917a8acd09279b58a1646e
CatalinaAlvarez <catalina.alvarez051@gmail.com>
2021-11-26 18:15:38 -0500

    Se borra el back para reemplazarlo por un nuevo back con las dependencias necesarias

--- back/src/main/java/co/com/sofka/crud/Repositories/TaskListRepository.java
+++
@@ -1,7 +0,0 @@
-package co.com.sofka.crud.Repositories;
-
-import co.com.sofka.crud.Models.TaskList;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TaskListRepository extends CrudRepository<TaskList, Long> {
-}
